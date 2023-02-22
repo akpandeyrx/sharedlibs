9f0a4833194a398cb269c7dee5a68b3fa73d9b5c
akpandeyrx <akpandey@amalgamrx.com>
2023-02-22 16:07:11 +0530

    Update calculate.groovy

--- vars/calculate.groovy
+++ vars/calculate.groovy
@@ -1,8 +1,8 @@
 def sum(x,y){
-  echo "sum of $x and $y is = $x+$y"
+  echo "sum of $x and $y is = ${x+y}"
 
 }
 
 def multi(x,y){
- echo "Multiplication of $x and $y is ==> $x*$y"
+  echo "Multiplication of $x and $y is ==> ${x*y}"
 }
